f24a35091c8966d329f5497e9d66cba56ce24a4c
ABDELHAMID-ELBOUZ <abdelhamidelbouz@gmail.com>
2024-11-08 10:49:18 +0100

    Update ft_lstsize_bonus.c

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -26,21 +26,3 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count);
 }
-#include <stdio.h>
-int main()
-{
-	t_list *head = NULL;
-	t_list *node1 = ft_lstnew("element 1");
-	t_list *node2 = ft_lstnew("element 2");
-	t_list *node3 = ft_lstnew("element 3");
-	t_list *node4 = ft_lstnew("element 4");
-	node1->next = node2;
-	node2->next = node3;
-	node3->next = node4;
-	node4->next = NULL;
-	int size = ft_lstsize(node1);
-	printf("%d\n",size);
-	ft_lstclear(&node1,free);
-	printf("%d\n",size);
-
-}
